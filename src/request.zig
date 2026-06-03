ac785ddd2ede18629b6cd569a8627de5ff46d0e5
SyoBoN <syobon@syobon.net>
2026-06-03 17:53:46 +0900

    rewrite with zig 0.16.0

--- src/request.zig
+++ src/request.zig
@@ -3,12 +3,14 @@
 // SPDX-License-Identifier: UPL-1.0
 
 const std = @import("std");
-const root = @import("root.zig");
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
 
-const Headers = root.Headers;
-const References = root.References;
-const XSstpPassThru = root.XSstpPassThru;
-const SecurityLevel = root.SecurityLevel;
+const common = @import("common.zig");
+const Headers = common.Headers;
+const References = common.References;
+const XSstpPassThru = common.XSstpPassThru;
+const SecurityLevel = common.SecurityLevel;
 
 pub const ParseError = error{
     OutOfMemory,
@@ -27,28 +29,28 @@ pub const RequestRaw = struct {
     references: References,
     x_sstp_passthru: XSstpPassThru,
 
-    pub fn deinit(self: *@This()) void {
-        self.headers_raw.deinit();
-        self.references.deinit();
-        self.x_sstp_passthru.deinit();
+    pub fn deinit(self: *@This(), allocator: Allocator) void {
+        self.headers.deinit(allocator);
+        self.references.deinit(allocator);
+        self.x_sstp_passthru.deinit(allocator);
     }
 };
 
-pub fn parseRaw(body: []const u8, allocator: std.mem.Allocator) ParseError!RequestRaw {
+pub fn parseRaw(allocator: Allocator, body: []const u8) ParseError!RequestRaw {
     var split = std.mem.tokenizeSequence(u8, body, "\r\n");
 
     const method_line = split.next() orelse return ParseError.InvalidBody;
 
-    const method = if (std.mem.startsWith(u8, method_line, "GET SHIORI/3.0"))
-        Method.get
+    const method: Method = if (std.mem.startsWith(u8, method_line, "GET SHIORI/3.0"))
+        .get
     else if (std.mem.startsWith(u8, body, "NOTIFY SHIORI/3.0"))
-        Method.notify
+        .notify
     else
         return ParseError.UnsupportedProtocol;
 
-    var headers = Headers.init(allocator);
-    var references = References.init(allocator);
-    var x_sstp_passthru = XSstpPassThru.init(allocator);
+    var headers: Headers = .empty;
+    var references: References = .empty;
+    var x_sstp_passthru: XSstpPassThru = .empty;
     while (split.next()) |header| {
         const colon_index = std.mem.indexOfScalar(u8, header, ':') orelse continue;
 
@@ -60,14 +62,14 @@ pub fn parseRaw(body: []const u8, allocator: std.mem.Allocator) ParseError!Reque
         const value = std.mem.trim(u8, value_untrimmed, &std.ascii.whitespace);
 
         if (std.mem.startsWith(u8, key, "Reference")) {
-            const refnum = std.mem.trimLeft(u8, key, "Reference");
+            const refnum = std.mem.trimStart(u8, key, "Reference");
             const parsed = std.fmt.parseUnsigned(u64, refnum, 10) catch continue;
-            try references.put(parsed, value);
+            try references.put(allocator, parsed, value);
         } else if (std.mem.startsWith(u8, key, "X-SSTP-PassThuru-")) {
-            const sstp_key = std.mem.trimLeft(u8, key, "X-SSTP-PassThuru-");
-            try x_sstp_passthru.put(sstp_key, value);
+            const sstp_key = std.mem.trimStart(u8, key, "X-SSTP-PassThuru-");
+            try x_sstp_passthru.put(allocator, sstp_key, value);
         } else {
-            try headers.put(key, value);
+            try headers.put(allocator, key, value);
         }
     }
 
@@ -79,9 +81,7 @@ pub fn parseRaw(body: []const u8, allocator: std.mem.Allocator) ParseError!Reque
     };
 }
 
-pub const SenderType = enum {
-    unknown,
-
+const SenderTypeTag = enum {
     internal,
     external,
     sakuraapi,
@@ -93,10 +93,25 @@ pub const SenderType = enum {
     communicate,
 };
 
-// TODO: openingとballoonは値を持つので、Tagged Unionにしたい
-pub const Status = enum {
-    unknown,
+pub const SenderType = packed struct {
+    internal: bool = false,
+    external: bool = false,
+    sakuraapi: bool = false,
+    embed: bool = false,
+    raise: bool = false,
+    property: bool = false,
+    plugin: bool = false,
+    sstp: bool = false,
+    communicate: bool = false,
+};
 
+pub const Status = struct {
+    flags: StatusFlags = .{},
+    opening: ?[]const []const u8 = null,
+    baloon: ?[]Baloon = null,
+};
+
+const StatusTag = enum {
     talking,
     choosing,
     minimizing,
@@ -105,13 +120,25 @@ pub const Status = enum {
     timecritical,
     nouserbreak,
     online,
-    opening,
-    balloon,
+};
+
+const StatusFlags = packed struct {
+    talking: bool = false,
+    choosing: bool = false,
+    minimizing: bool = false,
+    induction: bool = false,
+    passive: bool = false,
+    timecritical: bool = false,
+    nouserbreak: bool = false,
+    online: bool = false,
+};
+
+const Baloon = struct {
+    character: u32,
+    baloon: u32,
 };
 
 pub const Request = struct {
-    allocator: std.mem.Allocator,
-
     method: Method,
     headers_raw: Headers,
 
@@ -122,27 +149,29 @@ pub const Request = struct {
     // 拡張
     charset: ?[]const u8,
     security_level: ?SecurityLevel,
-    sender_type: ?[]SenderType,
+    sender_type: ?SenderType,
     security_origin: ?[]const u8,
-    status: ?[]Status,
+    status: ?Status,
     base_id: ?[]const u8,
     x_sstp_passthru: XSstpPassThru,
 
-    pub fn deinit(self: *@This()) void {
-        if (self.sender_type) |*sender_type| {
-            self.allocator.free(sender_type.*);
+    pub fn deinit(self: *@This(), allocator: Allocator) void {
+        if (self.status) |status| {
+            if (status.opening) |opening| {
+                allocator.free(opening);
+            }
+            if (status.baloon) |baloon| {
+                allocator.free(baloon);
+            }
         }
-        if (self.status) |*status| {
-            self.allocator.free(status.*);
-        }
-        self.headers_raw.deinit();
-        self.references.deinit();
-        self.x_sstp_passthru.deinit();
+        self.headers_raw.deinit(allocator);
+        self.references.deinit(allocator);
+        self.x_sstp_passthru.deinit(allocator);
     }
 };
 
-pub fn parse(body: []const u8, allocator: std.mem.Allocator) ParseError!Request {
-    var raw = try parseRaw(body, allocator);
+pub fn parse(allocator: std.mem.Allocator, body: []const u8) ParseError!Request {
+    const raw = try parseRaw(allocator, body);
 
     const sender = raw.headers.get("Sender") orelse {
         return ParseError.InvalidBody;
@@ -156,17 +185,25 @@ pub fn parse(body: []const u8, allocator: std.mem.Allocator) ParseError!Request 
 
     const charset = raw.headers.get("Charset");
 
-    const sender_type = if (raw.headers.get("SenderType")) |sender_type_raw| sender_type: {
-        var sender_types = try std.ArrayList(SenderType).initCapacity(allocator, 9);
+    const sender_type = if (raw.headers.get("SenderType")) |sender_type_raw| blk: {
+        var sender_type: SenderType = .{};
         var split = std.mem.splitScalar(u8, sender_type_raw, ',');
         while (split.next()) |sender_type_str| {
-            if (std.meta.stringToEnum(SenderType, sender_type_str)) |sender_type| {
-                try sender_types.append(allocator, sender_type);
-            } else {
-                try sender_types.append(allocator, SenderType.unknown);
+            if (std.meta.stringToEnum(SenderTypeTag, sender_type_str)) |tag| {
+                switch (tag) {
+                    .internal => sender_type.internal = true,
+                    .external => sender_type.external = true,
+                    .sakuraapi => sender_type.sakuraapi = true,
+                    .embed => sender_type.embed = true,
+                    .raise => sender_type.raise = true,
+                    .property => sender_type.property = true,
+                    .plugin => sender_type.plugin = true,
+                    .sstp => sender_type.sstp = true,
+                    .communicate => sender_type.communicate = true,
+                }
             }
         }
-        break :sender_type try sender_types.toOwnedSlice(allocator);
+        break :blk sender_type;
     } else null;
 
     const security_level = if (raw.headers.get("SecurityLevel")) |security_level|
@@ -176,27 +213,56 @@ pub fn parse(body: []const u8, allocator: std.mem.Allocator) ParseError!Request 
 
     const security_origin = raw.headers.get("SecurityOrigin");
 
-    const status = if (raw.headers.get("Status")) |status_raw| status: {
-        var statuses = try std.ArrayList(Status).initCapacity(allocator, 10);
+    const status = if (raw.headers.get("Status")) |status_raw| blk: {
+        var status: Status = .{};
         var split = std.mem.splitScalar(u8, status_raw, ',');
         while (split.next()) |status_str| {
-            if (std.meta.stringToEnum(Status, status_str)) |status| {
-                try statuses.append(allocator, status);
-            } else {
-                if (std.mem.startsWith(u8, status_str, "opening(")) {
-                    try statuses.append(allocator, Status.opening);
-                } else if (std.mem.startsWith(u8, status_str, "balloon(")) {
-                    try statuses.append(allocator, Status.balloon);
-                } else {
-                    try statuses.append(allocator, Status.unknown);
+            if (std.meta.stringToEnum(StatusTag, status_str)) |tag| {
+                switch (tag) {
+                    .talking => status.flags.talking = true,
+                    .choosing => status.flags.choosing = true,
+                    .minimizing => status.flags.minimizing = true,
+                    .induction => status.flags.induction = true,
+                    .passive => status.flags.passive = true,
+                    .timecritical => status.flags.timecritical = true,
+                    .nouserbreak => status.flags.nouserbreak = true,
+                    .online => status.flags.online = true,
                 }
+            } else if (std.mem.startsWith(u8, status_str, "opening(")) {
+                var opening: ArrayList([]const u8) = .empty;
+                defer opening.deinit(allocator);
+
+                const trimed_1 = std.mem.trimStart(u8, status_str, "opening(");
+                const trimed_2 = std.mem.trimEnd(u8, trimed_1, ")");
+                var iter = std.mem.splitScalar(u8, trimed_2, '/');
+                while (iter.next()) |item| {
+                    try opening.append(allocator, item);
+                }
+
+                status.opening = try opening.toOwnedSlice(allocator);
+            } else if (std.mem.startsWith(u8, status_str, "balloon(")) {
+                var baloon: ArrayList(Baloon) = .empty;
+                defer baloon.deinit(allocator);
+
+                const trimed_1 = std.mem.trimStart(u8, status_str, "baloon(");
+                const trimed_2 = std.mem.trimEnd(u8, trimed_1, ")");
+                var iter = std.mem.splitScalar(u8, trimed_2, '/');
+                while (iter.next()) |item| {
+                    const equal_index = std.mem.indexOfScalar(u8, item, '=') orelse continue;
+                    const cid_str = item[0..equal_index];
+                    const bid_str = item[equal_index + 1 ..];
+                    const cid = std.fmt.parseUnsigned(u32, cid_str, 10) catch continue;
+                    const bid = std.fmt.parseUnsigned(u32, bid_str, 10) catch continue;
+                    try baloon.append(allocator, .{ .character = cid, .baloon = bid });
+                }
+
+                status.baloon = try baloon.toOwnedSlice(allocator);
             }
         }
-        break :status try statuses.toOwnedSlice(allocator);
+        break :blk status;
     } else null;
 
     return .{
-        .allocator = allocator,
         .method = raw.method,
         .headers_raw = raw.headers,
         .sender = sender,
@@ -216,13 +282,14 @@ test "Test simple request parsing" {
     const allocator = std.testing.allocator;
 
     const body = "GET SHIORI/3.0\r\nCharset: UTF-8\r\nSender: SSP\r\nSenderType: internal,raise\r\nSecurityLevel: local\r\nStatus: choosing,balloon(0=0)\r\nID: OnFirstBoot\r\nBaseID: OnBoot\r\nReference0: 1\r\n\r\n";
-    var parsed = try parse(body, allocator);
-    defer parsed.deinit();
+    var parsed = try parse(allocator, body);
+    defer parsed.deinit(allocator);
 
     try std.testing.expectEqualStrings("UTF-8", parsed.charset.?);
     try std.testing.expectEqualStrings("SSP", parsed.sender);
-    try std.testing.expectEqualSlices(SenderType, &.{ SenderType.internal, SenderType.raise }, parsed.sender_type.?);
-    try std.testing.expectEqualSlices(Status, &.{ Status.choosing, Status.balloon }, parsed.status.?);
+    try std.testing.expectEqual(SenderType{ .internal = true, .raise = true }, parsed.sender_type.?);
+    try std.testing.expectEqual(StatusFlags{ .choosing = true }, parsed.status.?.flags);
+    try std.testing.expectEqualSlices(Baloon, &.{.{ .baloon = 0, .character = 0 }}, parsed.status.?.baloon.?);
     try std.testing.expectEqualStrings("OnFirstBoot", parsed.id);
     try std.testing.expectEqualStrings("OnBoot", parsed.base_id.?);
     try std.testing.expectEqualStrings("1", parsed.references.get(0).?);
